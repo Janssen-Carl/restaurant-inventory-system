33d4e0d5e544b421656c9e167c942f842fd4d91e
Janssen12 <jcmm2023-4700-61428@bicol-u.edu.ph>
2024-11-22 15:46:29 +0800

    commit initialCommit.java

--- initialCommit.java
+++ initialCommit.java
@@ -1,5 +1,5 @@
 class initialCommit {
     public static void main(String[] args) {
-        System.out.println("This is the initial commit");
+        System.out.println("This is the initial commit!");
     }
 }
